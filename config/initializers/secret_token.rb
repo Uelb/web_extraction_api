c978f7dbead04677161e0ad3a2ca3b3fb53afed9
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-03-31 03:21:18 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SsqlWebExtractionApi::Application.config.secret_key_base = '197722740999b6c6b8689c8fc07c12e11ce95c97266b4967d42f7f91b30d5bff4f9b80bb060572049a1461b02448e27d615350c613fec2a9771e8ebed5cac88e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SsqlWebExtractionApi::Application.config.session_store :cookie_store, key: '_ssql_web_extraction_api_session'
