7ca085ad2a1cf0e6fb832b50abe8869adc3f4c93
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-04-11 12:35:34 +0900

    add assets compilation config

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module SsqlWebExtractionApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails.root}/web-extraction/lib"
+    config.assets.precompile += ["*.js.coffee", "*.js", "*.css.scss"]
   end
 end
