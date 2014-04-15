e1181541e5b064d1bb046e903d6116fa5d7a50e2
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-04-15 16:37:32 +0900

    add nickname to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415060921) do
+ActiveRecord::Schema.define(version: 20140415073714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20140415060921) do
     t.text     "uid"
     t.string   "provider"
     t.string   "provider_token"
+    t.string   "nickname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
